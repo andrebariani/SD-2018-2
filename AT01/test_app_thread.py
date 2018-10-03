2b462431694709a4b2c1dd9cc56049c442540bca
Gabriel Rodrigues Rocha <gabrielrodrigues_rocha@hotmail.com>
2018-10-03 13:38:53 -0300

    Fixed thread CPU hogging issue

--- AT01/test_app_thread.py
+++ AT01/test_app_thread.py
@@ -5,7 +5,7 @@ import time
 import random
 import heapq
 import threading
-from Queue import Queue
+from queue import Queue
 
 data_heap = []
 
@@ -54,17 +54,23 @@ def sender(p, q):
             current_message_id = current_message_id + 1
             print ('sending %s' % message)
             sent = sock.sendto(message.encode(), ('224.0.0.1', 10000))
+        time.sleep(random.random()*5)
 
 def receiver(p, n, q):
+    global turn
     ack_list = []
     acked = 0
-    global turn
     while True:
+        print('MESSAGE QUEUE:')
         print(data_heap)
 
         first_pass = True
         popped = False
 
+
+        print ('\nwaiting to receive message')
+        data = sock.recv(1024).decode()
+        
         while first_pass or popped:
             if (len(data_heap) != 0):
                 top_message_time, top_message = data_heap[0]
@@ -80,9 +86,6 @@ def receiver(p, n, q):
             else:
                 popped = False
             first_pass = False
-
-        print ('\nwaiting to receive message')
-        data = sock.recv(1024).decode()
 
         if turn != 1:
             turn = turn - 1
@@ -95,18 +98,14 @@ def receiver(p, n, q):
 
         if message_info != 'ack':
             message_time = message_info
-            #if t < int(message_time):
-            #    print ('current time is less than message time: %d < %d', (t, int(message_time)))
-            #    t = int(message_time)
-            #else:
-            #    t = t + 1
             t = q.get()
             q.put(max(int(message_time), t) + 1)
             heapq.heappush(data_heap, (int(message_time), data))
-            print('sending ack to process %s with message %s' % (message_pid, message_id))
+            print('Sending ack to process %s with message %s' % (message_pid, message_id))
             sock.sendto(('ack-' + message_pid + '*' + message_id).encode(), ('224.0.0.1', 10000))
         else:
             ack_list.append(data)
+            print ('ACKNOWLEDGEMENT LIST:')
             print (ack_list)
 
         time.sleep(random.random()*5)
@@ -115,10 +114,6 @@ def receiver(p, n, q):
 
 # Receive/respond loop
 def rr_loop (t, p, n):
-    current_message_id = 0
-    ack_list = []
-    acked = 0
-    #turn = p
     t = int(str(t) + str(p))
     queue = Queue()
     queue.put(t)
@@ -129,73 +124,5 @@ def rr_loop (t, p, n):
     t_receiver.start()
     t_sender.join()
     t_receiver.join()
-    """
-    while True:
-        print(data_heap)
-
-        if turn == 1 and current_message_id <= 4:
-            t = t + 1
-            message = str(t) + '-' + str(p) + '*' + str(current_message_id)
-            current_message_id = current_message_id + 1
-            print ('sending %s' % message)
-            print(type(message.encode('utf-8')))
-            #for i in range(1,n+1):
-            #    sent = sock.sendto(message.encode(), ('127.0.0.1', 10000+i))
-            sent = sock.sendto(message.encode(), ('224.0.0.1', 10000))
-            #sent = sock.sendmsg(message.encode('utf-8'))
-
-        first_pass = True
-        popped = False
-
-        while first_pass or popped:
-            if (len(data_heap) != 0):
-                top_message_time, top_message = data_heap[0]
-                if len([x for x in ack_list if get_mid(x) == get_mid(top_message) and get_pid(x) == get_pid(top_message)]) == n:
-                    print('processed message with id %s from process %s' % (get_mid(top_message), get_pid(top_message)))
-                    heapq.heappop(data_heap)
-                    popped = True
-                    acked = acked + 1
-                    print('number of processed messages: %d' % acked)
-                    ack_list = [x for x in ack_list if get_mid(x) != get_mid(top_message) or get_pid(x) != get_pid(top_message)]
-                else:
-                    popped = False
-            else:
-                popped = False
-            first_pass = False
-
-        if turn != 1 and turn != 0:
-            turn = turn - 1
-        if turn == 1:
-            t_sender.start()
-            turn = turn - 1
-
-        print ('\nwaiting to receive message')
-        data = sock.recv(1024).decode()
-
-        message_info = get_info(data)
-        message_pid = get_pid(data)
-        message_id = get_mid(data)
-
-        print ('message_info: %s\nmessage_pid: %s\nthis pid: %d' % (message_info, message_pid, p))
-
-        if message_info != 'ack':
-            message_time = message_info
-            #if t < int(message_time):
-            #    print ('current time is less than message time: %d < %d', (t, int(message_time)))
-            #    t = int(message_time)
-            #else:
-            #    t = t + 1
-            t = max(int(message_time), t) + 1
-            heapq.heappush(data_heap, (int(message_time), data))
-            print('sending ack to process %s with message %s' % (message_pid, message_id))
-            sock.sendto(('ack-' + message_pid + '*' + message_id).encode(), ('224.0.0.1', 10000))
-        else:
-            ack_list.append(data)
-            print (ack_list)
-
-        time.sleep(random.random()*5)
-
-        print (data)
-    """
 
 rr_loop(int(sys.argv[1]),int(sys.argv[2]), int(sys.argv[3]))
